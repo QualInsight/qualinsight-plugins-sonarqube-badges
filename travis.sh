222c65cdad48957da535e62ad939b70a1587a29c
michel <michel.pawlak@gmail.com>
2016-04-30 01:24:04 +0200

    update travis-ci configuration

---
+++ travis.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ] 
+then
+	echo "Running SonarQube analysis for Pull Request '${TRAVIS_PULL_REQUEST}'"
+	mvn clean verify sonar:sonar \
+		-Dsonar.analysis.mode=preview \
+		-Dsonar.host.url=${SONAR_HOST_URL} \
+		-Dsonar.github.login=${GITHUB_LOGIN} \
+		-Dsonar.github.oauth=${GITHUB_TOKEN} \
+		-Dsonar.github.repository=${TRAVIS_REPO_SLUG} \
+		-Dsonar.github.pullRequest=${TRAVIS_PULL_REQUEST}
+elif [ "${TRAVIS_BRANCH}" = "master" ]
+then
+	echo "Running build and SonarQube analysis"
+	mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar \
+		-Dsonar.host.url=${SONAR_HOST_URL} \
+		-Dsonar.login=${SONAR_TOKEN}
+else
+	echo "Running build, no analysis for branch '${TRAVIS_BRANCH}'"
+	mvn clean verify
+fi
