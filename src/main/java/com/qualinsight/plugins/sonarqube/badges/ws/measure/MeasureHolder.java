65c99d654a42a21045818716b7c4f3d8c0b040fb
Willy <willy@gdn-commerce.com>
2017-09-10 21:03:50 +0700

    closes #2 Adds period parameter to measure API

--- src/main/java/com/qualinsight/plugins/sonarqube/badges/ws/measure/MeasureHolder.java
+++ src/main/java/com/qualinsight/plugins/sonarqube/badges/ws/measure/MeasureHolder.java
@@ -20,6 +20,8 @@
 package com.qualinsight.plugins.sonarqube.badges.ws.measure;
 
 import java.io.Serializable;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.NoSuchElementException;
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
@@ -50,6 +52,28 @@ public class MeasureHolder {
     private SVGImageColor backgroundColor = SVGImageColor.GRAY;
 
     /**
+     * Get period value by period index
+     *
+     * @param periods PeriodsValue obtained from {@link Metric}
+     * @param periodIndex Period Index that wants to be obtained. Possible values are 1, 2, or 3.
+     *
+     * @return String of metric value or <i>null</i>
+     */
+    private String getPeriodValueByPeriodIndex(PeriodsValue periods, int periodIndex) {
+        String periodTempValue = null;
+        for (PeriodValue periodvalue : periods.getPeriodsValueList()) {
+            if (periodvalue.hasIndex() && periodvalue.getIndex() == periodIndex) {
+                periodTempValue = periodvalue.getValue();
+                break;
+            }
+        }
+        if (periodTempValue == null) {
+            periodTempValue = periods.getPeriodsValue(0).getValue();
+        }
+        return periodTempValue;
+    }
+
+    /**
      * Constructs a MeasureHolder from a metric key.
      *
      * @param metricKey key used to retrieve the metric name for which the MeasureHolder is built
@@ -71,24 +95,40 @@ public class MeasureHolder {
      * Constructs a MeasureHolder from a Measure object.
      *
      * @param measure used to retrieve the metric name for which the MeasureHolder is built
+     * @param requestedPeriod used to retrieve which period requested for which value will be constructed
      */
     @SuppressWarnings("unchecked")
-    public MeasureHolder(final Measure measure) {
+    public MeasureHolder(final Measure measure, int requestedPeriod) {
+        final DecimalFormat valueDf = new DecimalFormat("#.##");
+        valueDf.setRoundingMode(RoundingMode.CEILING);
+        final DecimalFormat periodValueDf = new DecimalFormat("+#.##;-#.##");
+        periodValueDf.setRoundingMode(RoundingMode.CEILING);
+
         final Metric<Serializable> metric = CoreMetrics.getMetric(measure.getMetric());
         this.metricName = metric.getName()
             .replace(" (%)", "")
             .toLowerCase();
         String tempValue = null;
-        if (!measure.hasValue()) {
-            if (measure.hasPeriods()) {
-                final PeriodsValue periods = measure.getPeriods();
-                final PeriodValue periodValue = periods.getPeriodsValue(0);
-                tempValue = periodValue.getValue();
-            }
-        } else {
-            tempValue = measure.getValue();
+        String periodTempValue = null;
+        if (measure.hasPeriods() && requestedPeriod > 0) {
+            periodTempValue = this.getPeriodValueByPeriodIndex(measure.getPeriods(), requestedPeriod);
         }
-        this.value = tempValue == null ? NA : tempValue + (metric.isPercentageType() ? "%" : "");
+        tempValue = measure.getValue();
+
+        tempValue = tempValue != null ? valueDf.format(Double.parseDouble(tempValue)) : null;
+        periodTempValue = periodTempValue != null ? periodValueDf.format(Double.parseDouble(periodTempValue)) : null;
+
+        // Build value
+        // If metric has no value, period's value will be treated as value
+        // If metric has value, period's value will be treated as delta
+
+        if (tempValue == null) {
+            String percentage = metric.isPercentageType() ? "%" : "";
+            this.value = periodTempValue == null ? NA : periodTempValue + percentage;
+        } else {
+            this.value = tempValue + (metric.isPercentageType() ? "%" : "");
+            this.value = this.value + (periodTempValue != null ? " " + "(" + periodTempValue + ")" : "");
+        }
     }
 
     /**
